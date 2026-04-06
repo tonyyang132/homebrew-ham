6ccee21af005fd964a19c40e6f2c026dea22d414
Tony Yang <tonyyang132@gmail.com>
2026-04-06 01:20:32 -0700

    chirp 20260403

--- Casks/chirp.rb
+++ Casks/chirp.rb
@@ -1,6 +1,6 @@
 cask "chirp" do
-  version "20260123"
-  sha256 "841a4c2e2792f797da0a578fd0ca0f39cbfa57e3206763916b092fc76d14334c"
+  version "20260403"
+  sha256 "eeef045b5e6ac9f8bd702175ec41d0eb5d2ded0db66e8602f4fc320bc8f89760"
 
   url "https://archive.chirpmyradio.com/chirp_next/next-#{version}/chirp-next-#{version}.app.zip",
       referer:    "https://archive.chirpmyradio.com/chirp_next/next-#{version}/",
